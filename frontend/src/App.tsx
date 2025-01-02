985ae958afb3a77c97f06010c32ccdc467b5a6c2
kivccc <71814049+kivccc@users.noreply.github.com>
2025-01-02 19:50:03 +0900

    Revert "feat:소셜 로그인을 통한 사용자 데이터(코드) 확인"
    This reverts commit 4405d1f3bca0d5e1de455c3cada711ffbf3c40c2.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./components/Login/index";
-import Auth from "./components/Auth/index";
+import React, { useState } from 'react';
 
-const App = () => {
+const App: React.FC = () => {
+  const [response, setResponse] = useState<string>('');
+
+  const fetchGreeting = async () => {
+    try {
+      const res = await fetch('/api/greeting'); // API 요청
+      const res2 = await fetch('/api/greeting2'); // API 요청
+      console.log(res.body);
+      console.log(res2.body);
+      if (!res.ok) {
+        throw new Error('Failed to fetch');
+      }
+      const data: { message: string } = await res.json(); // JSON 데이터 파싱
+      setResponse(data.message); // 응답 메시지 상태에 저장
+    } catch (error) {
+      console.error('Error fetching greeting:', error);
+      setResponse('Failed to fetch greeting.');
+    }
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login/oauth2/callback/kakao" element={<Auth />} />
-    </Routes>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>React (TypeScript) to Spring Boot Example</h1>
+      <button onClick={fetchGreeting}>Fetch Greeting</button>
+      <p>{response}</p>
+    </div>
   );
-}
+};
 
 export default App;
